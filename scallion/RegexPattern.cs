bd35bbd29dd73911ca1ad85a4b781684ea1c76c8
freethenation <klafter719r@yahoo.com>
2012-10-13 00:50:07 -0400

    1) Started the code for supporting multiple patterns

---
+++ scallion/RegexPattern.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace scallion
+{
+	public class RegexPattern
+	{
+		private readonly List<char[]> _parsedRegex = new List<char[]>();
+		private readonly Regex _regex;
+		public RegexPattern(string regex)
+		{
+			regex = regex.ToLower();
+			_regex = new Regex(regex);
+			if (!Regex.IsMatch(regex, @"(\[[abcdefghijklmnopqrstuvwxyz234567]*\]|[abcdefghijklmnopqrstuvwxyz234567.])*"))
+				throw new System.ArgumentException("The passed regex string is not valid!");
+			_parsedRegex = 
+				Regex.Matches(regex, @"\[[abcdefghijklmnopqrstuvwxyz234567]*\]|[abcdefghijklmnopqrstuvwxyz234567.]")
+				.Cast<Match>()
+				.Select(match => match.Groups[0].Value.ToArray())
+				.ToList();
+		}
+		public IEnumerable<string> GeneratePatterns()
+		{
+			return GeneratePatterns(_parsedRegex);
+		}
+		private IEnumerable<string> GeneratePatterns(IEnumerable<char[]> remainingPattern)
+		{
+			foreach (char c in remainingPattern.First())
+				foreach (string s in GeneratePatterns(remainingPattern.Skip(1)))
+					yield return c + s;
+		}
+		//public IEnumerable<KeyValuePair<string, IEnumerable<string>>> GenerateBitmasksAndPatterns()
+		//{
+			
+		//}
+	}
+}
